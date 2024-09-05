fc3b99b4daf3e0577cd8ce1462453d975f513641
juligric <49377944@est.ort.edu.ar>
2024-09-05 12:01:00 -0300

    carrusel cais resuwewkto
    carrusel cais lo reoslvi me flata uqe se superoponga y que se achique

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.querySelectorAll('.categories button').forEach(button => {
+    button.addEventListener('click', toggleCarrusel);
+});
+
 const swiper = new Swiper('.mySwiper', {
     slidesPerView: 5,            // Muestra 5 imágenes a la vez
     spaceBetween: 30,            // Espacio entre las imágenes en píxeles
@@ -14,3 +18,21 @@ const swiper = new Swiper('.mySwiper', {
     loop: true,                  // Activa el loop para que el carrusel sea infinito
     freeMode: false              // Desactiva el modo libre
 });
+
+function toggleCarrusel() {
+    const carrusel = document.querySelector('.swiper-container');
+
+    if(carrusel.classList.contains('swiper-container-visible')){
+        carrusel.classList.toggle('swiper-container-hidden');
+        carrusel.classList.remove('swiper-container-visible');
+    }
+    else{
+        carrusel.classList.remove('swiper-container-hidden');
+        carrusel.classList.toggle('swiper-container-visible');
+    }
+   
+    if (carrusel.classList.contains('swiper-container-visible')) {
+        swiper.update(); // Actualiza Swiper para asegurar un funcionamiento correcto
+    }
+}
+
